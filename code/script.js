6e3aa4509096947816e8f7a601352a81eb9b1b6f
Moa Blomkvist <moa@birgerochblom.se>
2021-05-24 11:36:34 +0200

    minor changes

--- code/script.js
+++ code/script.js
@@ -21,26 +21,72 @@ console.dir(question1, question2)
 question1.onclick = () => {
   const answerContainer1 = question1.nextElementSibling
   answerContainer1.classList.toggle('active')
+
+  const answerContainer2 = question2.nextElementSibling
+  const answerContainer3 = question3.nextElementSibling
+  const answerContainer4 = question4.nextElementSibling
+  const answerContainer5 = question5.nextElementSibling
+  answerContainer2.classList.remove('active')
+  answerContainer3.classList.remove('active')
+  answerContainer4.classList.remove('active')
+  answerContainer5.classList.remove('active')
 }
 
 question2.onclick = () => {
   const answerContainer2 = question2.nextElementSibling
   answerContainer2.classList.toggle('active')
+  
+  //when the others expand, this goes up again)
+  const answerContainer1 = question1.nextElementSibling
+  const answerContainer3 = question3.nextElementSibling
+  const answerContainer4 = question4.nextElementSibling
+  const answerContainer5 = question5.nextElementSibling
+  answerContainer1.classList.remove('active')
+  answerContainer3.classList.remove('active')
+  answerContainer4.classList.remove('active')
+  answerContainer5.classList.remove('active')
 }
 
 question3.onclick = () => {
   const answerContainer3 = question3.nextElementSibling
   answerContainer3.classList.toggle('active')
+
+  const answerContainer1 = question1.nextElementSibling
+  const answerContainer2 = question2.nextElementSibling
+  const answerContainer4 = question4.nextElementSibling
+  const answerContainer5 = question5.nextElementSibling
+  answerContainer1.classList.remove('active')
+  answerContainer2.classList.remove('active')
+  answerContainer4.classList.remove('active')
+  answerContainer5.classList.remove('active')
 }
 
 question4.onclick = () => {
   const answerContainer4 = question4.nextElementSibling
   answerContainer4.classList.toggle('active')
+
+  const answerContainer1 = question1.nextElementSibling
+  const answerContainer2 = question2.nextElementSibling
+  const answerContainer3 = question3.nextElementSibling
+  const answerContainer5 = question5.nextElementSibling
+  answerContainer1.classList.remove('active')
+  answerContainer2.classList.remove('active')
+  answerContainer3.classList.remove('active')
+  answerContainer5.classList.remove('active')
 }
 
 question5.onclick = () => {
   const answerContainer5 = question5.nextElementSibling
   answerContainer5.classList.toggle('active')
+
+  const answerContainer1 = question1.nextElementSibling
+  const answerContainer2 = question2.nextElementSibling
+  const answerContainer3 = question3.nextElementSibling
+  const answerContainer4 = question4.nextElementSibling
+  answerContainer1.classList.remove('active')
+  answerContainer2.classList.remove('active')
+  answerContainer3.classList.remove('active')
+  answerContainer4.classList.remove('active')
 }
 
 
