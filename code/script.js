480f83d11b4212172e08e7ae288ef4d8ee9bc785
Moa Blomkvist <moa@birgerochblom.se>
2021-05-24 11:19:27 +0200

    structure .js in a better way

--- code/script.js
+++ code/script.js
@@ -2,13 +2,48 @@
 // We haven't really talked about ´this´ yet, but we will... ;)
 // console.log(this) to see how it works
 
-function toggle () {
-  this.classList.toggle("active")
-}
+// function toggle () {
+//   this.classList.toggle("active")
+// }
 
 // Selects an HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("question1").onclick = toggle
-document.getElementById("question2").onclick = toggle
-document.getElementById("question3").onclick = toggle
-document.getElementById("question4").onclick = toggle
-document.getElementById("question5").onclick = toggle
+
+const question1 = document.getElementById("question-1");
+const question2 = document.getElementById("question-2");
+const question3 = document.getElementById("question-3");
+const question4 = document.getElementById("question-4");
+const question5 = document.getElementById("question-5");
+
+console.log(question1)
+console.dir(question1, question2)
+
+
+question1.onclick = () => {
+  const answerContainer1 = question1.nextElementSibling
+  answerContainer1.classList.toggle('active')
+}
+
+question2.onclick = () => {
+  const answerContainer2 = question2.nextElementSibling
+  answerContainer2.classList.toggle('active')
+}
+
+question3.onclick = () => {
+  const answerContainer3 = question3.nextElementSibling
+  answerContainer3.classList.toggle('active')
+}
+
+question4.onclick = () => {
+  const answerContainer4 = question4.nextElementSibling
+  answerContainer4.classList.toggle('active')
+}
+
+question5.onclick = () => {
+  const answerContainer5 = question5.nextElementSibling
+  answerContainer5.classList.toggle('active')
+}
+
+
+
+
+
